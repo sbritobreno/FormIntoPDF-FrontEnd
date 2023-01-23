8798d95c64e874f47fa91fac8716a7d4842f6181
Breno Brito <breno.silvabrito@hotmail.com>
2023-01-23 19:32:56 +0000

    Create Flash message component

---
+++ src/components/layout/Message.js
@@ -0,0 +1,37 @@
+import styles from "./Message.module.css";
+import { useState, useEffect } from "react";
+import bus from "../../utils/bus";
+
+function Message() {
+  const [visibility, setVisibility] = useState(false);
+  const [message, setMessage] = useState("");
+  const [type, setType] = useState("");
+
+  useEffect(() => {
+    bus.addListener("flash", ({ message, type }) => {
+      setVisibility(true);
+      setMessage(message);
+      setType(type);
+
+      setTimeout(() => {
+        setVisibility(false);
+      }, 3000);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (document.querySelector(".close") !== null) {
+      document
+        .querySelector(".close")
+        .addEventListener("click", () => setVisibility(false));
+    }
+  });
+
+  return (
+    visibility && (
+      <div className={`${styles.message} ${styles[type]}`}>{message}</div>
+    )
+  );
+}
+
+export default Message;
