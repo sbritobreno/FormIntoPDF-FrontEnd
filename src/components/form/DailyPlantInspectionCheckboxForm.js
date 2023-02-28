7cbafeef7892b908de60a7b48cf4f2cb41932929
Breno Brito <breno.silvabrito@hotmail.com>
2023-02-28 10:13:29 +0000

    Create a DPICheckboxForm component and use it on Forms page

---
+++ src/components/form/DailyPlantInspectionCheckboxForm.js
@@ -0,0 +1,41 @@
+import styles from "./DailyPlantInspectionCheckboxForm.module.css";
+
+function DPICheckboxForm({ title, name }) {
+  return (
+    <div className={styles.dpi_checkbox_form}>
+      <h4>{title}:</h4>
+      <div className={styles.week_checkbox_form}>
+        <div className={styles.day_checkbox_form}>
+          <label>Mon</label>
+          <input type="checkbox" name={"dpi_" + name + "_mon"} />
+        </div>
+        <div className={styles.day_checkbox_form}>
+          <label>Tue</label>
+          <input type="checkbox" name={"dpi_" + name + "_tue"} />
+        </div>
+        <div className={styles.day_checkbox_form}>
+          <label>Wed</label>
+          <input type="checkbox" name={"dpi_" + name + "_wed"} />
+        </div>
+        <div className={styles.day_checkbox_form}>
+          <label>Thurs</label>
+          <input type="checkbox" name={"dpi_" + name + "_thurs"} />
+        </div>
+        <div className={styles.day_checkbox_form}>
+          <label>Fri</label>
+          <input type="checkbox" name={"dpi_" + name + "_fri"} />
+        </div>
+        <div className={styles.day_checkbox_form}>
+          <label>Sat</label>
+          <input type="checkbox" name={"dpi_" + name + "_sat"} />
+        </div>
+        <div className={styles.day_checkbox_form}>
+          <label>Sun</label>
+          <input type="checkbox" name={"dpi_" + name + "_sun"} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DPICheckboxForm;
--- src/components/pages/DocumentSection/Forms.js
+++ src/components/pages/DocumentSection/Forms.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../form/Input";
+import DPICheckboxForm from "../../form/DailyPlantInspectionCheckboxForm";
 import styles from "./Doc.module.css";
 
 function Forms() {
@@ -68,15 +69,22 @@ function Forms() {
         </div>
         <div className={styles.check_container}>
           <input type="checkbox" name="hot_work_precautions_box_4" />
-          <label>Gas cylinders secured in a vertical position on a trolley</label>
+          <label>
+            Gas cylinders secured in a vertical position on a trolley
+          </label>
         </div>
         <div className={styles.check_container}>
           <input type="checkbox" name="hot_work_precautions_box_5" />
-          <label>Gas cylinders fitted with a regulator and flashback arrestor</label>
+          <label>
+            Gas cylinders fitted with a regulator and flashback arrestor
+          </label>
         </div>
         <div className={styles.check_container}>
           <input type="checkbox" name="hot_work_precautions_box_6" />
-          <label>Other personnel who may be affected by the work removed from the area</label>
+          <label>
+            Other personnel who may be affected by the work removed from the
+            area
+          </label>
         </div>
         <div className={styles.check_container}>
           <input type="checkbox" name="hot_work_precautions_box_7" />
@@ -170,6 +178,37 @@ function Forms() {
           handleOnChange={handleChange}
           autoComplete="off"
         />
+        <h2 className={styles.form_subheading}>Daily Plant Inspection</h2>
+        <DPICheckboxForm title={"1. Flashing Beacon"} name={"flashing_beacon"}/>
+        <DPICheckboxForm title={"2. Mirrors"} name={"mirrors"}/>
+        <DPICheckboxForm title={"3. Reverse Camera"} name={"reverse_camera"}/>
+        <DPICheckboxForm title={"4. Wipers"} name={"wipers"}/>
+        <DPICheckboxForm title={"5. Lights"} name={"lights"}/>
+        <DPICheckboxForm title={"6. Horn"} name={"horn"}/>
+        <DPICheckboxForm title={"7. Lab belt"} name={"lab_belt"}/>
+        <DPICheckboxForm title={"8. Steps"} name={"steps"}/>
+        <DPICheckboxForm title={"9. Hand Rails"} name={"hand_rails"}/>
+        <DPICheckboxForm title={"10. Emergency Stop"} name={"emergency_stop"}/>
+        <DPICheckboxForm title={"11. Hand Brake"} name={"hand_brake"}/>
+        <DPICheckboxForm title={"12. Brakes"} name={"brakes"}/>
+        <DPICheckboxForm title={"13. Drivers Seat"} name={"drivers_seat"}/>
+        <DPICheckboxForm title={"14. Windows"} name={"windows"}/>
+        <DPICheckboxForm title={"15. Fire Extinguisher"} name={"fire_extinguisher"}/>
+        <DPICheckboxForm title={"16. Guards in Place"} name={"guards_in_place"}/>
+        <DPICheckboxForm title={"17. Gas Bottles Secure"} name={"gas_bottles_secure"}/>
+        <DPICheckboxForm title={"18. Battery Condition"} name={"battery_condition"}/>
+        <DPICheckboxForm title={"19. Tyres"} name={"tyres"}/>
+        <DPICheckboxForm title={"20. Steering"} name={"steering"}/>
+        <DPICheckboxForm title={"21. Quick Hitch Pin"} name={"quick_hitch_pin"}/>
+        <DPICheckboxForm title={"22. FOPS"} name={"fops"}/>
+        <DPICheckboxForm title={"23. ROPS"} name={"rops"}/>
+        <DPICheckboxForm title={"24. Straps in good condition"} name={"straps_in_good_condition"}/>
+        <DPICheckboxForm title={"25. Transmission Oil"} name={"transmission_oil"}/>
+        <DPICheckboxForm title={"26. Engine Oil"} name={"engine_oil"}/>
+        <DPICheckboxForm title={"27. Hydraulic Oil"} name={"hydraulic_oil"}/>
+        <DPICheckboxForm title={"28. Water"} name={"water"}/>
+        <DPICheckboxForm title={"29. Fuel"} name={"fuel"}/>
+        <DPICheckboxForm title={"30. Brake Fluid"} name={"brake_fluid"}/>
         <input type="submit" value="Submit" />
       </form>
     </section>
